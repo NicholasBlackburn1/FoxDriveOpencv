a29f76fb9399229f347f0f99d8b6d20bd20362e8
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-10-28 21:04:42 -0400

    getting base stuff working

---
+++ foxdriveopencv/__main__.py
@@ -0,0 +1,18 @@
+"""
+main uwu
+"""
+from utils import logger
+import cv2
+
+def main():
+    logger.info("starting Computer Rec....")
+
+    logger.PipeLine_Data("opencv version "+cv2.__version__)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
